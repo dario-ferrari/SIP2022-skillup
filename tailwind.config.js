5fac4c3670d1e66befccbc3f2b458ae9efc55938
dario-ferrari <57733048+dario-ferrari@users.noreply.github.com>
2022-08-24 20:43:42 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFD28E",
+        secondary: "#FF8E8E",
+      },
+    },
+  },
+  plugins: [],
+};
